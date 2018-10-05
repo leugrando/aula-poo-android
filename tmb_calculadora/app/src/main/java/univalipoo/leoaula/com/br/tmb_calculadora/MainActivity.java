40e15513e777a03c308132a2294f3d80bd39cb5b
unknown <leugrando@hotmail.com>
2018-10-04 23:34:57 -0300

    Calculadora

---
+++ tmb_calculadora/app/src/main/java/univalipoo/leoaula/com/br/tmb_calculadora/MainActivity.java
@@ -0,0 +1,13 @@
+package univalipoo.leoaula.com.br.tmb_calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
